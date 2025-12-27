8b892a72cc8eb53b22f5ca152662fbe7b7c7a400
Siva Kona <131153324+Usersiva@users.noreply.github.com>
2025-12-27 16:27:41 +0530

    Import Message component in ChatWindow

--- ChatWindow.jsx
+++ ChatWindow.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import Message from "./Message";
 
 export default function ChatWindow() {
   const [messages, setMessages] = useState([]);
